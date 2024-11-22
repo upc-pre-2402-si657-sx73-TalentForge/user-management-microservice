f806d59df4841775eb548e68d4d952e2d92a97d7
Juan Diego Huaman Monzón <u202123965@upc.edu.pe>
2024-11-22 18:52:29 -0500

    feat(Cors): Add CorsConfigurer

---
+++ src/main/java/com/acme/fromzeroapi/shared/infraestructure/documentation/openapi/configuration/WebConfig.java
@@ -0,0 +1,24 @@
+package com.acme.fromzeroapi.shared.infraestructure.documentation.openapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://fromzero-77cf7.web.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Content-Type", "Authorization")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
