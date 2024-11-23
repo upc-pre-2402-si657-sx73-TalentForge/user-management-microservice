08a84d7e0caa08ca777add25971fecda6316e1bb
Juan Diego Huaman Monzón <u202123965@upc.edu.pe>
2024-11-22 19:00:03 -0500

    feat(Cors): fix CorsConfigurer

--- src/main/java/com/acme/fromzeroapi/shared/infraestructure/documentation/openapi/configuration/WebConfiguration.java
+++ src/main/java/com/acme/fromzeroapi/shared/infraestructure/documentation/openapi/configuration/WebConfiguration.java
@@ -2,11 +2,13 @@ package com.acme.fromzeroapi.shared.infraestructure.documentation.openapi.config
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@Profile("!test")
+public class WebConfiguration {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
